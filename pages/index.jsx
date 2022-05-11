72cb65f6eb32788bb4af89168c905e26d1557e42
Atul <atuldedha20@gmail.com>
2022-05-11 21:54:03 +0530

    First Commit

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Delivery Site</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        Delivery Website
+      </div>
+    </div>
+  )
+}
+
+export default Home
